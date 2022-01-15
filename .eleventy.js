6f921df4a78d0f2da1bcbe5f515381352e63aba8
Justin Ahinon <justiny.ahinon@gmail.com>
2022-01-15 20:18:28 +0100

    Add 404 page and BrowserSync config for serving it locally

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,26 @@
+const fs = require("fs");
+
 module.exports = eleventyConfig => {
     eleventyConfig.addPassthroughCopy("src/assets");
+
+    // BrowserSync config for handling 404s locally
+    eleventyConfig.setBrowserSyncConfig({
+        callbacks: {
+            ready: function (err, bs) {
+
+                bs.addMiddleware("*", (req, res) => {
+                    const content_404 = fs.readFileSync("dist/404.html");
+
+                    // Add 404 http status code in request header.
+                    res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+
+                    // Provides the 404 content without redirect.
+                    res.write(content_404);
+                    res.end();
+                });
+            }
+        }
+    });
 
     return {
         markdownTemplateEngine: 'njk',
